d0156ff48fa805a7813ccb55aacb8db6564da515
Dhruv Upadhyay <1997dhruv@gmail.com>
2016-11-01 23:58:25 -0400

    add Firebase cocoapod

---
+++ Pods/Target Support Files/Pods-No-More-Clowns/Pods-No-More-Clowns-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_No_More_Clowns : NSObject
+@end
+@implementation PodsDummy_Pods_No_More_Clowns
+@end
---
+++ Pods/Target Support Files/Pods-No-More-Clowns/Pods-No-More-Clowns-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_No_More_ClownsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_No_More_ClownsVersionString[];
+
